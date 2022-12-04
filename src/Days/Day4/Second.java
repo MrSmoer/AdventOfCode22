409f46c168ed2b8c9cd4fed21f897e975125a85a
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2022-12-04 19:28:50 +0100

    Done day4

---
+++ src/Days/Day4/Second.java
@@ -0,0 +1,50 @@
+package Days.Day4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Second {
+
+    public static void main(String[] args) {
+
+        String[] zeilen = readLines("src/Days/Day4/input.txt");
+        //String[] zeilen = readLines("src/Days/Day4/testInput.txt");
+        int score = 0;
+        for (String zeile : zeilen){
+            String[] ranges = zeile.split(",");
+            Section first = convertRangeToSection(ranges[0]);
+            Section second = convertRangeToSection(ranges[1]);
+            if(first.overlaps(second)||second.overlaps(first))
+                score++;
+        }
+        System.out.println(score);
+         // 737 to low
+
+
+    }
+
+    public static Section convertRangeToSection(String range){
+        String[] numbers = range.split("-");
+        return new Section(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
+    }
+
+    public static String[] readLines(String filename) {
+        try (FileReader fileReader = new FileReader(filename)) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            List<String> lines = new ArrayList<String>();
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+            bufferedReader.close();
+            return lines.toArray(new String[lines.size()]);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return new String[1];
+        }
+    }
+}
---
+++ src/Days/Day4/Section.java
@@ -0,0 +1,40 @@
+package Days.Day4;
+
+public class Section {
+    int begin;
+    int end;
+
+    public Section(int pBegin, int pEnd) {
+        this.begin = pBegin;
+        this.end = pEnd;
+    }
+
+    public boolean fullyContains(Section otherSection) {
+        if (otherSection.begin >= this.begin && otherSection.end <= this.end)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean overlaps(Section otherSection) {
+        int tBeg = this.begin;
+        int tEnd = this.end;
+        int oBeg = otherSection.begin;
+        int oEnd = otherSection.end;
+        // contains begin
+        if (oBeg >= tBeg && oBeg <= tEnd)
+            return true;
+        // contains end
+        if (oEnd >= tBeg && oEnd <= tEnd)
+            return true;
+        return false;
+    }
+
+    public int getBegin() {
+        return begin;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
