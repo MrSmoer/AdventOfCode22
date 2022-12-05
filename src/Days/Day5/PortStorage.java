fa6a8c6996de951c5087cc9d911ba39b7385bcdd
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2022-12-05 22:28:01 +0100

    Day 5 done

---
+++ src/Days/Day5/PortStorage.java
@@ -0,0 +1,41 @@
+package Days.Day5;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class PortStorage {
+    Deque<Character>[] storDeques;
+    PortStorage(int size){
+        this.storDeques = (ArrayDeque<Character>[])new ArrayDeque[size];
+    }
+
+    public void addStack(int stackNumber, Deque<Character> stack){
+        this.storDeques[stackNumber]=stack;
+    }
+
+    public void addToStack(int stackNumber,char item){
+        if(this.storDeques[stackNumber-1].add(item))
+        ;//System.out.println("Success!");
+        else System.out.println("No success");
+    }
+
+    public Character removeTopFromStack(int stackNumber){
+        Character c = this.storDeques[stackNumber-1].removeLast();
+
+        return c;
+    }
+
+    public void moveItemFromStackToStack(int firstStack, int secondStack){
+        addToStack(secondStack, removeTopFromStack(firstStack));
+
+    }
+
+    public String getTopBoxes(){
+        String result = "";
+        for(Deque<Character> stack : this.storDeques){
+            result=result+stack.peekLast();
+        }
+        return result;
+    }
+
+}
