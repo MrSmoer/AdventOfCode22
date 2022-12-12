aa664bbe0e310f8f594e238a82e1d45c3f394d38
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2022-12-12 18:45:13 +0100

    day 12 part1

--- src/Days/Day12/MapPoint.java
+++ src/Days/Day12/MapPoint.java
@@ -2,7 +2,50 @@ package Days.Day12;
 
 import java.awt.Point;
 
+
 public class MapPoint {
     Point position;
     boolean visitedBefore;
+    char height;
+    int stepCount;
+
+    public MapPoint(char height){
+        this.height=height;
+    }
+
+    public int getStepCount() {
+        return stepCount;
+    }
+
+    public void setStepCount(int stepCount) {
+        this.stepCount = stepCount;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    public boolean isVisitedBefore() {
+        return visitedBefore;
+    }
+
+    public void setVisitedBefore(boolean visitedBefore) {
+        this.visitedBefore = visitedBefore;
+    }
+
+    public char getHeight() {
+        return height;
+    }
+
+    public void setHeight(char height) {
+        this.height = height;
+    }
+
+    
+
+    
 }
